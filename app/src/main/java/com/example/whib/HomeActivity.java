99a4d1d6842804d222d3ad45342b0d3d19202162
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-29 23:56:34 +0200

    W2 Finalized

--- app/src/main/java/com/example/whib/HomeActivity.java
+++ app/src/main/java/com/example/whib/HomeActivity.java
@@ -1,17 +1,40 @@
 package com.example.whib;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Set OnClickListener
+        Button toMyWhereaboutsScreenButton = findViewById(R.id.toMyWhereaboutsScreenButton);
+        toMyWhereaboutsScreenButton.setOnClickListener(this);
+
+        Button toMyRemindersScreenButton = findViewById(R.id.toMyRemindersScreenButton);
+        toMyRemindersScreenButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        // Check what button is pressed; Change activity accordingly
+        switch(v.getId()) {
+            case R.id.toMyWhereaboutsScreenButton:
+                Intent toMyWhereaboutsScreenIntent = new Intent(this, TestActivity.class);
+                startActivity(toMyWhereaboutsScreenIntent);
+                break;
+            case R.id.toMyRemindersScreenButton:
+                Intent toMyRemindersScreenIntent = new Intent(this, TestActivity.class);
+                startActivity(toMyRemindersScreenIntent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/whib/TestActivity.java
+++ app/src/main/java/com/example/whib/TestActivity.java
@@ -3,12 +3,27 @@ package com.example.whib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        // Set OnClickListener
+        ImageView toPreviousScreenButton = findViewById(R.id.actionbarBackButton);
+        toPreviousScreenButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        // Check what button is pressed; Change activity accordingly
+        switch(v.getId()) {
+            case R.id.actionbarBackButton:
+                onBackPressed();
+                break;
+        }
     }
 }
