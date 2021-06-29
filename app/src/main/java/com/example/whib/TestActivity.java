9494eb16a41f4fdada984ee13ac1b1b118381cf3
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-29 20:20:45 +0200

    W1 Welcome Screen + Test Screen

--- app/src/main/java/com/example/whib/TestActivity.java
+++ app/src/main/java/com/example/whib/TestActivity.java
@@ -3,13 +3,12 @@ package com.example.whib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
     }
 }
