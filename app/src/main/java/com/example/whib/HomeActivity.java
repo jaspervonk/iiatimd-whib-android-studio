7690af44bbf9139cd1806a089acb4b9193ea4a85
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-29 22:34:53 +0200

    W2 Base Functionalities
    - Logo Image added
    - Back Image added
    - HomeActivity added
    - actionbar_home added
    - actionbar_default added

---
+++ app/src/main/java/com/example/whib/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.whib;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/whib/WelcomeActivity.java
+++ app/src/main/java/com/example/whib/WelcomeActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,12 +16,18 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        // Set OnClickListener
         Button toHomeScreenButton = findViewById(R.id.toHomeScreenButton);
         toHomeScreenButton.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        Intent toHomeScreenIntent = new Intent(this, TestActivity.class);
-        startActivity(toHomeScreenIntent);
+        // Check what button is pressed; Change activity accordingly
+        switch(v.getId()) {
+            case R.id.toHomeScreenButton:
+                Intent toHomeScreenIntent = new Intent(this, HomeActivity.class);
+                startActivity(toHomeScreenIntent);
+                break;
+        }
     }
 }
