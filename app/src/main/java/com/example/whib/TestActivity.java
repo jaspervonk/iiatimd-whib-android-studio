39c908ce455029e7f34c9b253b4d8cd0fd50ec02
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-29 23:59:38 +0200

    TextActivity actionbarTitle changed

--- app/src/main/java/com/example/whib/TestActivity.java
+++ app/src/main/java/com/example/whib/TestActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class TestActivity extends AppCompatActivity implements View.OnClickListener {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        // Set actionbar title
+        TextView actionbarTitle = findViewById(R.id.actionbarTitle);
+        actionbarTitle.setText("Test Page");
 
         // Set OnClickListener
         ImageView toPreviousScreenButton = findViewById(R.id.actionbarBackButton);
